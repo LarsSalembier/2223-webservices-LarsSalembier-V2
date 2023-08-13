3929c8a9e75fa0e7df0c1aa13e66c3751b463cc7
larssalembier <lars.salembier@gmail.com>
2023-08-14 01:44:56 +0200

    Exploration of Koa

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@ import Koa from 'koa';
 
 const app = new Koa();
 
-app.use(async (ctx) => {
+app.use(async (ctx, next) => {
   ctx.body = 'Hello World';
+  await next();
+});
+
+app.use(async (ctx, next) => {
+  console.log(ctx);
+  await next();
 });
 
 app.listen(9000);
