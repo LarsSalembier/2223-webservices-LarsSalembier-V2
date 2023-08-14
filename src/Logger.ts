0d54a656306b54f19d1bbba507f635e3bb17a592
larssalembier <lars.salembier@gmail.com>
2023-08-14 15:42:47 +0200

    Made logger behave differently in prod

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,6 +1,21 @@
 import winston from 'winston';
+import config from 'config';
+
+enum LogLevel {
+  ERROR = 'error',
+  WARN = 'warn',
+  INFO = 'info',
+  HTTP = 'http',
+  VERBOSE = 'verbose',
+  DEBUG = 'debug',
+  SILLY = 'silly',
+}
 
 const LOG_DIRECTORY = 'logs';
+const LOG_LEVEL: LogLevel = config.get('log.level');
+const LOG_TO_CONSOLE: boolean = config.get('log.toConsole');
+const LOG_TO_FILE: boolean = config.get('log.toFile');
+const LOG_ENABLED: boolean = config.get('log.enabled');
 
 const customFormat = winston.format.printf(
   ({ level, message, label, timestamp }) => {
@@ -14,23 +29,35 @@ class Logger {
   private logger: winston.Logger;
 
   private constructor() {
+    const transports: winston.transport[] = [];
+
+    // Add file transports if logging to file is enabled
+    if (LOG_TO_FILE) {
+      transports.push(
+        new winston.transports.File({
+          filename: `${LOG_DIRECTORY}/error.log`,
+          level: LogLevel.ERROR,
+        }),
+        new winston.transports.File({
+          filename: `${LOG_DIRECTORY}/combined.log`,
+        })
+      );
+    }
+
+    // Add console transport if logging to console is enabled
+    if (LOG_TO_CONSOLE) {
+      transports.push(new winston.transports.Console());
+    }
+
     this.logger = winston.createLogger({
-      level: 'info',
+      level: LOG_LEVEL,
       format: winston.format.combine(
         winston.format.label({ label: 'MAIN' }),
         winston.format.timestamp(),
         customFormat
       ),
       defaultMeta: { service: 'user-service' },
-      transports: [
-        new winston.transports.File({
-          filename: `${LOG_DIRECTORY}/error.log`,
-          level: 'error',
-        }),
-        new winston.transports.File({
-          filename: `${LOG_DIRECTORY}/combined.log`,
-        }),
-      ],
+      transports,
     });
   }
 
@@ -42,32 +69,38 @@ class Logger {
     return Logger.instance;
   }
 
-  public info(message: string): void {
-    this.logger.info(message);
+  public log(level: LogLevel, message: string): void {
+    if (LOG_ENABLED) {
+      this.logger.log(level, message);
+    }
   }
 
   public error(message: string): void {
-    this.logger.error(message);
+    this.log(LogLevel.ERROR, message);
   }
 
   public warn(message: string): void {
-    this.logger.warn(message);
+    this.log(LogLevel.WARN, message);
   }
 
-  public debug(message: string): void {
-    this.logger.debug(message);
+  public info(message: string): void {
+    this.log(LogLevel.INFO, message);
+  }
+
+  public http(message: string): void {
+    this.log(LogLevel.HTTP, message);
   }
 
   public verbose(message: string): void {
-    this.logger.verbose(message);
+    this.log(LogLevel.VERBOSE, message);
+  }
+
+  public debug(message: string): void {
+    this.log(LogLevel.DEBUG, message);
   }
 
   public silly(message: string): void {
-    this.logger.silly(message);
-  }
-
-  public log(level: string, message: string): void {
-    this.logger.log(level, message);
+    this.log(LogLevel.SILLY, message);
   }
 }
 
