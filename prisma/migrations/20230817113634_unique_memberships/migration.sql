83796f9c0f0a2fc945c0873d285d33ac33081b1e
larssalembier <lars.salembier@gmail.com>
2023-08-17 20:49:51 +0200

    Simplified migrations since there is no data yet

--- prisma/migrations/20230817113634_unique_memberships/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[personId,groupId]` on the table `Membership` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Membership_personId_groupId_key" ON "Membership"("personId", "groupId");
--- prisma/migrations/20230817170412_base/migration.sql
+++ prisma/migrations/20230817170412_base/migration.sql
@@ -1,11 +1,4 @@
 -- CreateTable
-CREATE TABLE "Article" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "content" TEXT NOT NULL
-);
-
--- CreateTable
 CREATE TABLE "Person" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
@@ -22,37 +15,28 @@ CREATE TABLE "Group" (
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "color" TEXT,
-    "ageRange" TEXT,
-    "sex" TEXT
+    "target" TEXT
 );
 
 -- CreateTable
 CREATE TABLE "Membership" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "personId" INTEGER NOT NULL,
     "groupId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("personId", "groupId"),
     CONSTRAINT "Membership_personId_fkey" FOREIGN KEY ("personId") REFERENCES "Person" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "Membership_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
-CREATE TABLE "Event" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "image" TEXT,
-    "startDateTime" DATETIME NOT NULL,
-    "endDateTime" DATETIME,
-    "location" TEXT NOT NULL
-);
-
--- CreateTable
 CREATE TABLE "Administrator" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "auth0id" TEXT NOT NULL PRIMARY KEY,
     "username" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
     "email" TEXT NOT NULL
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Membership_personId_groupId_key" ON "Membership"("personId", "groupId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Administrator_username_key" ON "Administrator"("username");
